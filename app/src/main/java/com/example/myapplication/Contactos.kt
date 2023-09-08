5d1edaf1fd2074359ba0c5866502a56cb570a467
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-09-08 12:56:12 -0600

    ajustess

--- app/src/main/java/com/example/myapplication/Contactos.kt
+++ app/src/main/java/com/example/myapplication/Contactos.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,8 +19,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class Contactos : ComponentActivity() {
@@ -41,7 +45,6 @@ class Contactos : ComponentActivity() {
 @Composable
 fun Greeting() {
     Column {
-
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
@@ -51,14 +54,22 @@ fun Greeting() {
                 contentDescription = null,
                 tint = Color.Unspecified,
                 modifier = Modifier.padding(end = 50.dp)
+                    .size(40.dp),
             )
 
-            Text(text = "Contactos", modifier = Modifier.align(Alignment.CenterVertically))
+            Text(
+                text = "Contactos", fontFamily = FontFamily.Serif, textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
+        }
+        Text(
+            text = "Acá toca hacer barra de busquedad", fontFamily = FontFamily.Serif, textAlign = TextAlign.Center,
+            fontSize = 20.sp)
+
+
         }
 
-    }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
