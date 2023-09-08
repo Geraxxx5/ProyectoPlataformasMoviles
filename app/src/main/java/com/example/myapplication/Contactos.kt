ec8fd5ed7d09749f4f39b7049886db6c6c16d6a9
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-09-08 12:04:04 -0600

    ajustes

--- app/src/main/java/com/example/myapplication/Contactos.kt
+++ app/src/main/java/com/example/myapplication/Contactos.kt
@@ -1,14 +1,18 @@
 package com.example.myapplication
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -30,7 +34,14 @@ class Contactos : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    
+Row {
+    Icon(
+        painter = painterResource(R.drawable.baseline_menu_24),
+        contentDescription = null
+    )
+
+    Text(text = "Contactos")
+}
 
 }
 
