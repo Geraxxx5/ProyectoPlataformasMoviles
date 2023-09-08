3c0ca200bd9d81738b136b45b226ab95fb0f1a86
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-09-08 12:14:05 -0600

    Cambios

--- app/src/main/java/com/example/myapplication/Contactos.kt
+++ app/src/main/java/com/example/myapplication/Contactos.kt
@@ -4,16 +4,22 @@ import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class Contactos : ComponentActivity() {
@@ -34,15 +40,23 @@ class Contactos : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-Row {
-    Icon(
-        painter = painterResource(R.drawable.baseline_menu_24),
-        contentDescription = null
-    )
+    Column {
 
-    Text(text = "Contactos")
-}
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.baseline_menu_24),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.padding(end = 50.dp)
+            )
 
+            Text(text = "Contactos", modifier = Modifier.align(Alignment.CenterVertically))
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
