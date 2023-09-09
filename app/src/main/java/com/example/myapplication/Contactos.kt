72d5622ce7e3809a64f99e1080376023b5c29637
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-09-08 18:47:21 -0600

    creación de lo buscador

--- app/src/main/java/com/example/myapplication/Contactos.kt
+++ app/src/main/java/com/example/myapplication/Contactos.kt
@@ -1,29 +1,55 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.leanback.widget.SearchBar
+import androidx.leanback.widget.SearchBar.SearchBarListener
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class Contactos : ComponentActivity() {
@@ -42,6 +68,7 @@ class Contactos : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting() {
     Column {
@@ -53,7 +80,8 @@ fun Greeting() {
                 painter = painterResource(R.drawable.baseline_menu_24),
                 contentDescription = null,
                 tint = Color.Unspecified,
-                modifier = Modifier.padding(end = 50.dp)
+                modifier = Modifier
+                    .padding(end = 50.dp)
                     .size(40.dp),
             )
 
@@ -62,14 +90,75 @@ fun Greeting() {
                 fontSize = 20.sp
             )
         }
-        Text(
-            text = "Acá toca hacer barra de busquedad", fontFamily = FontFamily.Serif, textAlign = TextAlign.Center,
-            fontSize = 20.sp)
-
-
-        }
-
+        MainSreen()
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainSreen(){
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    var items = remember {
+        mutableStateListOf(
+            "Banco Industrial",
+            "Banco G&T"
+        )
+    }
+    Scaffold {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = text ,
+            onQueryChange = {
+                text = it
+            } ,
+            onSearch = {
+                items.add(text)
+                active = false
+                text = ""
+            },
+            active = active ,
+            onActiveChange ={
+                active = it
+            },
+            placeholder = {
+                Text(text = "Buscar Contacto")
+            },
+            leadingIcon = {
+              Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+            },
+            trailingIcon = {
+                if (active) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                           if(text.isNotEmpty()) {
+                               text = ""
+                           } else {
+                            active= false
+                           }
+                        },
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close Icon"
+                    )
+                }
+            }
+        ) {
+    items.forEach{
+        Row (
+            modifier = Modifier.padding(all = 14.dp)
+        ){
+            Icon(
+                modifier = Modifier.padding(end = 10.dp),
+                imageVector = Icons.Default.History, contentDescription = "Historial " 
+            )
+            Text(text = it)
+        }
+    }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
