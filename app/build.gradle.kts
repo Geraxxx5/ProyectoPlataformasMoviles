52e1a1f85fb8b4201b81e1c0af105e10999fe58e
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-11-05 12:14:56 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
 }
 dependencies {
     implementation ("androidx.compose:compose-runtime:1.1.0")
-    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
-    implementation ("com.google.dagger:hilt-android:2.44")
+    implementation ("androidx.hilt:hilt-lifecycle-view-model:1.0.0-alpha03")
+    implementation ("com.google.dagger:hilt-android:2.48")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
